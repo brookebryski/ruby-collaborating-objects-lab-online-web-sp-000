c63e9c81c7eef132e8b4da9ee26e05b3271a430e
chic-state-0895 <brookeabryski@gmail.com>
2020-09-20 18:45:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+=begin
 class Song
 
   attr_accessor :name, :artist
@@ -25,9 +26,42 @@ class Song
   song
   end
 
-  def artist_name=
+  def artist_name=(name)
     artist = Artist.find_or_create_by_name
     self.artist = artist
+    artist.add_song(self)
+  end
+end
+=end
+class Song
+
+  attr_accessor :name, :artist
+
+    @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+    def save
+     @@all << self
+    end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
 end
